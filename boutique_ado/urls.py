0fefb504b1b66a4e09926d74d1cea12b1d585295
enocol <eno_col@yahoo.com>
2025-06-06 22:43:19 +0100

    Edit url.py and home.html

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -17,11 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def ignore_well_known_request(request):
+    return HttpResponse(status=204)  # or 404 with empty body
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')), 
     path('accounts/', include('allauth.urls')),
+    path('.well-known/appspecific/com.chrome.devtools.json', ignore_well_known_request), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
