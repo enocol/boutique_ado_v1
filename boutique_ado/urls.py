5255a970992a8fe59cf823ca2faf1145565eed78
enocol <eno_col@yahoo.com>
2025-06-08 12:22:39 +0100

    Add product details functionality

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')), 
     path('accounts/', include('allauth.urls')),
+    path('products/', include('products.urls')),
     path('.well-known/appspecific/com.chrome.devtools.json', ignore_well_known_request), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Product
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 # Create your views here.
+
+def all_products(request):
+    """ A view to return all products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/all_products.html', context)
+
+def product_detail(request, product_id):
+    """ A view to return a single product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+        'star_range': [1, 2, 3, 4, 5, 6],  # Assuming a 5-star rating system
+
+    }
+
+    return render(request, 'products/product_detail.html', context)
