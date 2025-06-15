7f2f299f9274a5f9dea5238423044344ceb5547e
enocol <eno_col@yahoo.com>
2025-06-15 08:13:29 +0100

    Enable sorting and searching

--- products/views.py
+++ products/views.py
@@ -1,56 +1,73 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Product
 from django.shortcuts import get_object_or_404
-from django.db.models import Q
 from django.contrib import messages
+from django.db.models import Q
+
+from django.db.models.functions import Lower
 
 # Create your views here.
 
 def all_products(request):
-    """ A view to return all products """
-
+    """ A view to return all products with sorting, searching and filtering """
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
+    sortkey = None
 
-
+    # Search logic
     if 'q' in request.GET:
         query = request.GET['q']
         if query:
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-            categories = products.filter(category__name__icontains=query)
         else:
             messages.error(request, "You didn't enter any search criteria!")
             return redirect(reverse('all_products'))
 
-       
-        
-    
-
+    # Category filtering
     if 'category' in request.GET:
-        categories = request.GET['category'].split(',')
-        print('categories===', categories)
-        
-        if categories:
-            products = products.filter(category__name__in=categories)
-            categories = Product.objects.filter(category__name__in=categories).distinct()
-        else:
+        categories = request.GET.get('category').split(',')
+        if not categories:
             messages.error(request, "You didn't select any categories!")
             return redirect(reverse('all_products'))
-        
        
-       
+        if categories:
+            products = products.filter(category__name__in=categories)
+
+    # Sorting logic
+    if 'sort' in request.GET:
+        sort = request.GET['sort']
+        direction = request.GET.get('direction', 'asc')
         
-        
+        if sort == 'name':
+            sortkey = 'name'
+        elif sort == 'price':
+            sortkey = 'price'
+        elif sort == 'rating':
+            sortkey = 'rating'
+        elif sort == 'category':
+            sortkey = 'category__name'
+        else:
+            messages.error(request, "Invalid sort option!")
+            return redirect(reverse('all_products'))
+
+        if sortkey:
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
     context = {
         'products': products,
-        # 'categories': categories,
-        # 'star_range': range(1, 6),  # Add this line so you can loop from 1 to 5 in your template
+        'current_sort': f'{sort}_{direction}' if sort else '',
+        'categories': categories,
     }
 
     return render(request, 'products/all_products.html', context)
+
+
 
 def product_detail(request, product_id):
     """ A view to return a single product """
