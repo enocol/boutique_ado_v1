d6c0cf07bdd2473cbba2fd8df1542384c04205ec
enocol <eno_col@yahoo.com>
2025-06-29 12:12:02 +0100

    Add javascript to update quantity of product in bag

--- bag/views.py
+++ bag/views.py
@@ -29,15 +29,20 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
           if size in bag[item_id]['items_by_size'].keys():
             bag[item_id]['items_by_size'][size] += quantity
+            messages.success(request, f'Added {quantity} of size {size} of item {item_id} to your bag.')
           else:
             bag[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Added {quantity} of size {size} of item {item_id} to your bag.')
         else:
            bag[item_id] = {'items_by_size': {size: quantity}}
+        messages.success(request, f'Added {quantity} of size {size} of item {item_id} to your bag.')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Added {quantity} of item {item_id} to your bag.')
         else:
             bag[item_id] = quantity 
+            messages.success(request, f'Added {quantity} of item {item_id} to your bag.')
     redirect_url = request.POST.get('redirect_url')
     
     # Save the updated session back to the request
@@ -77,16 +82,33 @@ def adjust_bag(request, item_id):
     return redirect('bag')
 
 
-def remove_from_bag(request, item_id):
-    """ Remove the item from the shopping bag """
-    bag = request.session.get('bag', {})    
+def remove_from_bag(request, item_id, size=None):
+    """Remove a specific size of an item from the shopping bag, or the whole item if no size is specified."""
+    bag = request.session.get('bag', {})
+    item_id = str(item_id)
+    size = request.GET.get('size', None)  # Get size from query parameters if not provided
+
     if item_id in bag:
-        del bag[item_id]
-        messages.success(request, f'Removed item {item_id} from your bag.')
+        if size:
+            # Only remove the specific size
+            if size in bag[item_id]['items_by_size']:
+                del bag[item_id]['items_by_size'][size]
+                messages.success(request, f"Removed size {size.upper()} of item {item_id} from your bag.")
+
+                # If no sizes left, remove the item completely
+                if not bag[item_id]['items_by_size']:
+                    del bag[item_id]
+                    messages.success(request, f"All sizes of item {item_id} removed from your bag.")
+            else:
+                messages.warning(request, f"Size {size.upper()} of item {item_id} not found in your bag.")
+        else:
+            # If no size is given, remove the whole item
+            del bag[item_id]
+            messages.success(request, f"Removed item {item_id} from your bag.")
     else:
-        messages.error(request, f'Item {item_id} not found in your bag.')   
-    # Save the updated session back to the request
+        messages.error(request, f"Item {item_id} not found in your bag.")
+
     request.session['bag'] = bag
-    return redirect('bag')
+    return redirect('bag')  # Replace with your actual bag view name
 
 
